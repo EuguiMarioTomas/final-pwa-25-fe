c566516cc779dd34f83197ba921ae7cf5ff6abe7
tomas eugui <mariotomaseugui@gmail.com>
2025-07-29 22:35:53 -0300

    refactor: agregar dueDate para mostrar en card

--- src/pages/getReminders/myReminders.tsx
+++ src/pages/getReminders/myReminders.tsx
@@ -4,9 +4,6 @@ import {useAuth} from '../../context/auth/useAuth';
 import {useNavigate} from 'react-router-dom';
 
 
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 interface Reminder {
   _id: string;
   title: string;
@@ -88,7 +85,7 @@ function MyReminders() {
                   <h2 className='card-title'>{reminder.title}</h2>
                   <p className='card-text'>{reminder.description}</p>
                   <p className='card-text'><strong>Tipo:</strong> {reminder.type}</p>
-                  {reminder.dueDate && (<p className='card-text'><strong>Vence:</strong> {new Date(reminder.dueDate).toLocaleDateString()}</p>)}
+                  {reminder.dueDate && (<p className='card-text'><strong>Vence:</strong> {reminder.dueDate?.substring(0, 10).split('-').reverse().join('/')}</p>)}
                   <div className='d-flex justify-content-between mt-3'>
                     <button className='btn btn-sm btn-outline-primary' onClick={() => handleEdit(reminder._id)}>Editar</button>
                     <button className='btn btn-sm btn-outline-primary' onClick={() => handleShareReminder(reminder._id)}>Compartir</button>
