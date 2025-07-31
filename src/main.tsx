87ab90eb1100653fb690b659e0a089f53f313b1c
tomas eugui <mariotomaseugui@gmail.com>
2025-07-31 18:08:30 -0300

    feat: importacion bootstrap y estilos css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/auth/AuthProvider';
 
+import './index.css';
+
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AuthProvider>
