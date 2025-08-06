a19215186bf9d055d58cdcb61848f8ce262ca78d
tomas eugui <mariotomaseugui@gmail.com>
2025-08-06 03:19:57 -0300

    feat: actualizar disposicion inputs

--- src/pages/createReminders/createReminder.tsx
+++ src/pages/createReminders/createReminder.tsx
@@ -36,32 +36,30 @@ function CreateReminder() {
   };
 
   return(
-    <div className='container create-reminder-container'>
+    <div className='container create-reminder-container mt-5'>
       <h1 className='create-title mb-4'>Crear Recordatorio</h1>
       <form onSubmit={handleSubmit(onSubmit)} className='reminder-form'>
-        <div className='mb-3'>
-          <label className='form-label'>Título</label>
-          <input className='form-control' {...register('title', {required:true})}/>
-          {errors.title && <span className='text-danger'>El título es obligatorio.</span>}
-        </div>
-        <div className='mb-3'>
-          <label className='form-label'>Descripción</label>
-          <textarea className='form-control' rows={3} {...register('description', {required:true})}/>
-          {errors.description && <span className='text-danger'>La descripción es obligatoria.</span>}
-        </div>
-        <div className='mb-3'>
-          <label className='form-label'>Tipo</label>
-          <select className='form-select' {...register('type', {required:true})}>
-            <option value='Pendiente'>Pendiente</option>
-            <option value='Evento'>Evento</option>
-            <option value='Idea'>Idea</option>
-            <option value='Otro'>Otro</option>
-          </select>
-          {errors.type && <span className='text-danger'>El tipo es obligatorio.</span>}
-        </div>
-        <div className='mb-3'>
-          <label className='form-label'>Fecha de vencimiento</label>
-          <input className='form-control' type='date' {...register('dueDate')}/>
+        <div className='row'>
+          <div className='mb-3 col-8'>
+            <input className='form-control' placeholder='🏷️Título... ej.Supermercado' {...register('title', {required:true})}/>
+            {errors.title && <span className='text-danger'>El título es obligatorio.</span>}
+          </div>
+          <div className='mb-3 col-4'>
+            <select className='form-select' {...register('type', {required:true})}>
+              <option value='Pendiente'>Pendiente</option>
+              <option value='Evento'>Evento</option>
+              <option value='Idea'>Idea</option>
+              <option value='Otro'>Otro</option>
+            </select>
+            {errors.type && <span className='text-danger'>El tipo es obligatorio.</span>}
+          </div>
+          <div className='mb-3 col-8'>
+            <textarea className='form-control' placeholder='✍🏽 Descripción... ej.Comprar mayonesa' rows={3} {...register('description', {required:true})}/>
+            {errors.description && <span className='text-danger'>La descripción es obligatoria.</span>}
+          </div>
+          <div className='mb-3 col-4'>
+            <input className='form-control py-3 mt-2' type='date' {...register('dueDate')}/>
+          </div>
         </div>
         <div className='mt-auto d-flex justify-content-end mt-3'>
           <button className='btn btn-custom' type='submit'>Crear Recordatorio</button>
